dc6f2d3ed4286b6713112435eab1bcbb3fd67862
Wesley Junio <wesjrpiri@gmail.com>
2026-03-18 22:32:15 +0000

    feat: add login component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,12 @@
-import BookCalendar from "@/components/book-calendar"
-import TodayBooks from "@/components/today-books"
 import Header from "@/components/header"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import Statistics from "@/components/statistics"
-import BookingsPanel from "@/components/bookings"
+import LoginComponent from "@/components/login-component"
 
 export default function Page() {
 
   return (
     <>
       <Header />
-      <Statistics/>
-      <Tabs defaultValue="list" className="space-y-6">
-        <div className="pt-6 px-6">
-          <TabsList>
-            <TabsTrigger value="list">List of bookings</TabsTrigger>
-            <TabsTrigger value="calendar">Calendar</TabsTrigger>
-            <TabsTrigger value="today">Today</TabsTrigger>
-          </TabsList>
-
-        </div>
-
-        <TabsContent value="list">
-          <BookingsPanel />
-        </TabsContent>
-
-        <TabsContent value="calendar">
-          <BookCalendar key='calendar' />
-        </TabsContent>
-
-        <TabsContent value="today">
-          <TodayBooks />
-        </TabsContent>
-      </Tabs>
+      <LoginComponent/>
     </>
   )
 }
