3c0c5e7e1e873702da242b83911e97139dbec6f3
Wesley Junio <wesjrpiri@gmail.com>
2026-04-14 20:02:37 +0100

    feat: add clerk provider tag around the main component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
 
 export const metadata: Metadata = {
   title: 'bAppoint',
@@ -14,17 +15,20 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <style>{`
+    <ClerkProvider>
+
+      <html lang="en">
+        <head>
+          <style>{`
 html {
   font-family: ${GeistSans.style.fontFamily};
   --font-sans: ${GeistSans.variable};
   --font-mono: ${GeistMono.variable};
 }
         `}</style>
-      </head>
-      <body>{children}</body>
-    </html>
+        </head>
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
