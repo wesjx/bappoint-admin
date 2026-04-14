696b1a66cbb69bb7726e21949ab65ade7545fc69
Wesley Junio <wesjrpiri@gmail.com>
2026-04-14 20:01:16 +0100

    feat: add clerk user logged in.

--- components/header.tsx
+++ components/header.tsx
@@ -1,25 +1,30 @@
 import { Car, Settings } from "lucide-react";
 import { Badge } from "./ui/badge";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import Link from "next/link";
+import { Show, UserButton } from "@clerk/nextjs";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 export default function () {
   return (
     <header className="px-4 lg:px-6 h-16 flex items-center bg-white border-b border-slate-200">
       <div className="flex items-center">
-        <Car className="h-8 w-8 text-blue-600" />
-        <span className="ml-2 text-xl font-bold text-slate-900">CleanCar Pro</span>
+        <span className="ml-2 text-xl font-bold text-slate-900">bAppoint.ie</span>
         <Badge variant="secondary" className="ml-3 bg-blue-100 text-blue-800">
           Admin
         </Badge>
       </div>
       <div className="ml-auto flex items-center space-x-4">
-        <span className="text-sm text-slate-600">Welcome, Admin</span>
-        <Avatar>
-          <AvatarImage src="/placeholder.svg?height=32&width=32" alt="Admin" />
-          <AvatarFallback>AD</AvatarFallback>
-        </Avatar>
+      <div>
+          <Show when="signed-out">
+          </Show>
+          <Show when="signed-in">
+            <UserButton showName appearance={{
+              elements: {
+                userButtonBox: "max-w-[140px]"
+              }
+            }} />
+          </Show>
+        </div>
         <Button size="icon">
           <Link href='/admin/settings'>
             <Settings />
