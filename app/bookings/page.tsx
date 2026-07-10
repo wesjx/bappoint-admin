4b573a207987930a045cbec8066e9412717c4b52
Wesley Junio <wesjrpiri@gmail.com>
2026-07-10 23:04:21 +0100

    fix: remove coments to use components.

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,49 +1,39 @@
-"use client"
-import BookCalendar from "@/components/book-calendar"
-import TodayBooks from "@/components/today-books"
-import Header from "@/components/header"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import Statistics from "@/components/statistics"
-import BookingsPanel from "@/components/bookings"
-import { useCompany } from "@/contexts/company-context"
+"use client";
 
+import BookCalendar from "@/components/book-calendar";
+import TodayBooks from "@/components/today-books";
+import Header from "@/components/header";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Statistics from "@/components/statistics";
+import BookingsPanel from "@/components/bookings";
 
 export default function Bookings() {
-
-  const { company, loading } = useCompany();
-
-  console.log(company);
-
   return (
     <>
       <Header />
-      <Statistics/>
+      <Statistics />
+
       <Tabs defaultValue="list" className="space-y-6">
-        <div className="pt-6 px-6">
+        <div className="px-6 pt-6">
           <TabsList>
             <TabsTrigger value="list">List of bookings</TabsTrigger>
             <TabsTrigger value="calendar">Calendar</TabsTrigger>
             <TabsTrigger value="today">Today</TabsTrigger>
           </TabsList>
-
         </div>
 
-        {/* <TabsContent value="list">
+        <TabsContent value="list">
           <BookingsPanel />
         </TabsContent>
 
         <TabsContent value="calendar">
-          <BookCalendar key='calendar' />
+          <BookCalendar />
         </TabsContent>
 
         <TabsContent value="today">
           <TodayBooks />
-        </TabsContent> */}
+        </TabsContent>
       </Tabs>
-      <div>
-      <h1>{company?.name}</h1>
-      <p>{company?.email}</p>
-    </div>
     </>
-  )
+  );
 }
