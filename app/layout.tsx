6faaa94ff35eca40901d6a22170b88a2e2597ab8
Wesley Junio <wesjrpiri@gmail.com>
2026-07-30 18:01:11 +0100

    feat: fix build.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
-import './globals.css'
+import '@/app/globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { CompanyProvider } from '@/contexts/company-context'
 import { Toaster } from "sonner";
